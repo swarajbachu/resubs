9707b6d770892307b3fce5b1053254be656b01b3
Swaraj Bachu <rajswaraj.r@gmail.com>
2024-09-21 12:37:21 +0530

    format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import { SubscriptionTracker } from "@/components/subscription-tracker";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center ">
-      <ExpenseBreakdown />
-      <SubscriptionTracker />
-    </main>
-  );
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-center ">
+			<ExpenseBreakdown />
+			<SubscriptionTracker />
+		</main>
+	);
 }
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -5,32 +5,32 @@ import NextAuth from "next-auth";
 import type { NextAuthConfig } from "next-auth";
 import { eq } from "drizzle-orm";
 export const {
-  handlers: { GET, POST },
-  signIn,
-  signOut,
-  auth,
+	handlers: { GET, POST },
+	signIn,
+	signOut,
+	auth,
 } = NextAuth({
-  trustHost: true,
-  secret: process.env.AUTH_SECRET,
-  adapter: DrizzleAdapter(db, {
-    usersTable: user,
-    accountsTable: account,
-    sessionsTable: session,
-    verificationTokensTable: verificationTokens,
-  }),
-  events: {
-    async linkAccount({ user: AdapterUser }) {
-      await db
-        .update(user)
-        .set({
-          emailVerified: new Date(),
-        })
-        .where(eq(user.id, AdapterUser.id as string));
-    },
-  },
-  providers: [
-    Google({
-      allowDangerousEmailAccountLinking: true,
-    }),
-  ],
+	trustHost: true,
+	secret: process.env.AUTH_SECRET,
+	adapter: DrizzleAdapter(db, {
+		usersTable: user,
+		accountsTable: account,
+		sessionsTable: session,
+		verificationTokensTable: verificationTokens,
+	}),
+	events: {
+		async linkAccount({ user: AdapterUser }) {
+			await db
+				.update(user)
+				.set({
+					emailVerified: new Date(),
+				})
+				.where(eq(user.id, AdapterUser.id as string));
+		},
+	},
+	providers: [
+		Google({
+			allowDangerousEmailAccountLinking: true,
+		}),
+	],
 } as NextAuthConfig);
